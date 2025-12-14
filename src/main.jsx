208c59097d4112ae8af54b30cf35eef7e75edcf6
elmohsiao <elmohsiao@elmohsiaodeMac-mini.local>
2025-12-15 00:47:56 +0800

    Add 404.html for SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
+
+// 處理 404 重定向
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, null, redirect);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
